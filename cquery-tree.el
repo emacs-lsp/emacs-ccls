299e274f7a46acbed5fa8bed670610c4b87cd1c2
Fangrui Song <i@maskray.me>
2018-03-03 18:37:32 -0800

    Add cquery-tree--calling

--- cquery-tree.el
+++ cquery-tree.el
@@ -24,6 +24,8 @@
 
 (require 'cquery-common)
 
+(require 'xref)
+
 ;; ---------------------------------------------------------------------
 ;;   Customization
 ;; ---------------------------------------------------------------------
@@ -88,6 +90,9 @@
 
 (defvar-local cquery-tree--cur-client nil
   "Buffer tree client.")
+
+(defvar-local cquery-tree--calling t
+  "When non-nil, visit the node when the selected line changes.")
 
 (defun cquery-tree--read-node (data &optional parent)
   (funcall (cquery-tree-client-read-node-f cquery-tree--cur-client) data parent))
@@ -238,24 +243,46 @@
       (setf (cquery-tree-node-expanded node) t)
       (cquery-tree--refresh))))
 
-(defun cquery-tree-go ()
-  "Go to the definition of the function at point"
+(defun cquery-tree-toggle-calling ()
+  "Toggle `cquery-tree-calling'."
   (interactive)
-  (-when-let* ((node (cquery-tree--node-at-point)))
-    (select-window cquery-tree--origin-win)
-    (find-file (car (cquery-tree-node-location node)))
-    (goto-char (lsp--position-to-point (cdr (cquery-tree-node-location node))))
-    (pulse-momentary-highlight-one-line (point) 'next-error)))
+  (when (setq cquery-tree--calling (not cquery-tree--calling))
+    (cquery-tree-press)))
 
-(defun cquery-tree-look ()
-  "Look at the definition of function at point"
+(defun cquery-tree-press ()
+  "Jump to the location."
   (interactive)
-  (-when-let* ((node (cquery-tree--node-at-point)))
+  (-when-let* ((workspace lsp--cur-workspace)
+               (node (cquery-tree--node-at-point)))
     (with-selected-window cquery-tree--origin-win
       (find-file (car (cquery-tree-node-location node)))
+      ;; TODO Extract lsp-ui-peek.el lsp-ui-peek--goto-xref
+      (unless lsp--cur-workspace
+        (setq lsp--cur-workspace workspace))
+      (unless lsp-mode
+        (lsp-mode 1)
+        (lsp-on-open))
       (goto-char (lsp--position-to-point (cdr (cquery-tree-node-location node))))
       (recenter)
-      (pulse-momentary-highlight-one-line (point) 'next-error))))
+      (run-hooks 'xref-after-jump-hook))))
+
+(defun cquery-tree-press-and-switch ()
+  "Switch window and jump to the location."
+  (interactive)
+  (cquery-tree-press)
+  (select-window cquery-tree--origin-win))
+
+(defun cquery-tree-next-line (&optional arg)
+  (interactive "p")
+  (forward-line arg)
+  (when cquery-tree--calling
+    (cquery-tree-press)))
+
+(defun cquery-tree-prev-line (&optional arg)
+  (interactive "p")
+  (forward-line (- arg))
+  (when cquery-tree--calling
+    (cquery-tree-press)))
 
 (defun cquery-tree-expand-or-set-root ()
   "If the node at point is unexpanded expand it, otherwise set it as root"
@@ -279,17 +306,21 @@
 ;;   Mode
 ;; ---------------------------------------------------------------------
 
-(defvar cquery-tree-mode-map nil
-  "Keymap used with ‘cquery-tree-mode’.")
-
-(let ((map (make-sparse-keymap)))
-  (define-key map (kbd "<tab>") 'cquery-tree-toggle-expand)
-  (define-key map [mouse-1] 'cquery-tree-toggle-expand )
-  (define-key map (kbd "<return>") 'cquery-tree-go)
-  (define-key map (kbd "C-<return>") 'cquery-tree-look)
-  (define-key map (kbd "<left>") 'cquery-tree-collapse-or-select-parent)
-  (define-key map (kbd "<right>") 'cquery-tree-expand-or-set-root)
-  (setq cquery-tree-mode-map map))
+(defvar cquery-tree-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<tab>") 'cquery-tree-toggle-expand)
+    (define-key map [mouse-1] 'cquery-tree-toggle-expand)
+    (define-key map (kbd "c") 'cquery-tree-toggle-calling)
+    (define-key map (kbd "f") 'cquery-tree-press)
+    (define-key map (kbd "h") 'cquery-tree-collapse-or-select-parent)
+    (define-key map (kbd "j") 'cquery-tree-next-line)
+    (define-key map (kbd "k") 'cquery-tree-prev-line)
+    (define-key map (kbd "l") 'cquery-tree-expand-or-set-root)
+    (define-key map (kbd "<return>") 'cquery-tree-press-and-switch)
+    (define-key map (kbd "<left>") 'cquery-tree-collapse-or-select-parent)
+    (define-key map (kbd "<right>") 'cquery-tree-expand-or-set-root)
+    map)
+  "Keymap for `cquery-tree-mode'.")
 
 (define-derived-mode cquery-tree-mode special-mode "cquery-tree"
   "Mode for cquery tree buffers")
