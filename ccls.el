1b04d4d4f5c09e873e6728fa64a5245e3e2f4994
Nikita Bloshchanevich <nikblos@outlook.com>
2020-11-05 13:12:09 +0100

    Fix `ccls--show-xrefs'
    `ccls--show-xrefs' (and by extension and refs lenses) now actually works,
    instead of throwing a type error.

--- ccls.el
+++ ccls.el
@@ -145,9 +145,9 @@ DIRECTION can be \"D\", \"L\", \"R\" or \"U\"."
                                 ccls-root-files))
          (expand-file-name dir))))
 
-(lsp-defun ccls--show-xrefs ((&Command :arguments?))
+(lsp-defun ccls--show-xrefs ((&Command :command :arguments?))
   (when-let ((xrefs (lsp--locations-to-xref-items
-                     (lsp--send-execute-command command arguments))))
+                     (lsp--send-execute-command command arguments?))))
     (lsp-show-xrefs xrefs nil t)))
 
 (advice-add 'lsp--suggest-project-root :before-until #'ccls--suggest-project-root)
